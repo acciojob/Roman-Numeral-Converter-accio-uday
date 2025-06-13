584a2f5818c7e6335e5edcd3b477603e51cddfc5
accio-uday <udaymanideep2002@gmail.com>
2025-06-13 17:19:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function convertToRoman(num) {
+  // Array of Roman numerals and their corresponding values in descending order	
   	const obj = {
       0:['M',1000], 
       1:['D', 500], 
@@ -10,14 +11,19 @@ function convertToRoman(num) {
     };
 
   //your code here
+let result = "";
 
+for (let [symbol, value] of obj) {
+	while (num >= value) {
+		result += symbol;
+		num -= value;
+	}
+}
+	return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
-
-
-
 
 // do not edit below this line
 module.exports = convertToRoman
